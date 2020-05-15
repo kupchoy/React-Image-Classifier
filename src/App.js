1f8350b17cf188d67f51b82a7f55aa665a0172d1
kupchoy <kup.choy@gmail.com>
2020-05-15 11:38:52 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Classifier from './components/Classifier/Classifier';
+import ImageList from './components/ImageList/ImageList';
+import Navigation from './components/Navigation/Navigation';
+import { Route, BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navigation />
+      <div className='App'>
+      <Route exact path='/' component = {Classifier} />
+      <Route path='/history' component = {ImageList}/>
+      </div>
+    </BrowserRouter>
   );
 }
 
